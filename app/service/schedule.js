681d136d3e43ad8faaeb72039cd5d9c5ac25a113
Thomas Hunter II <me@thomashunter.name>
2017-05-29 19:45:51 -0700

    server init stuff

--- app/service/schedule.js
+++ app/service/schedule.js
@@ -1,4 +1,13 @@
-// Cron stuff
+/**
+ * Creates scheduler jobs for setting bank power
+ */
 const cron = require('node-cron');
 
+const config = require('../models/config.js');
+
 module.exports = {};
+
+module.exports.init = (callback) => {
+  // TODO
+  callback(null);
+};
--- powerbox.js
+++ powerbox.js
@@ -1,6 +1,26 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 
 const express = require('express');
+
+const schedule = require('./app/service/schedule.js');
+const config = require('./app/models/config.js');
+
+const controller_banks = require('./app/controllers/api/banks.js');
+const controller_config = require('./app/controllers/api/config.js');
+const controller_admin = require('./app/controllers/admin.js');
+
+const PORT = process.env.PORT || 80;
+const HOST = process.env.HOST || '0.0.0.0';
+
+const controllers = {
+  banks: controller_banks,
+  config: controller_config,
+  admin: controller_admin
+};
+
+console.log("Executing Powerbox...");
 
 if (process.platform !== 'linux' && !process.env.POWERBOX_DEBUG) {
   console.error("Powerbox currently only supports Linux");
@@ -9,21 +29,36 @@ if (process.platform !== 'linux' && !process.env.POWERBOX_DEBUG) {
 
 const timezone = fs.readFileSync('/etc/timezone').toString().trim();
 console.log(`Current System Timezone: ${timezone}`);
-console.log("Execute `tzconfig` if this looks wrong\n");
+console.log("Execute `tzconfig` if this looks wrong");
 
 const app = express();
 
-const PORT = process.env.PORT || 80;
-const HOST = process.env.HOST || '0.0.0.0';
+// TODO: Remove pyramid of doom
+config.db.init((error) => {
+  if (error) {
+    console.error("Unable to initialize configuration", error);
+    return void process.exit(100);
+  }
 
-const schedule = require('./app/service/schedule.js');
+  console.log("Database connection initialized.");
 
-const controllers = {
-  banks: require('./app/controllers/api/banks.js'),
-  config: require('./app/controllers/api/config.js')
-};
+  schedule.init((error) => {
+    if (error) {
+      console.error("Unable to initialize schedule", error);
+      return void process.exit(101);
+    }
 
-app.listen(PORT, HOST, (err) => {
-  console.log(`LISTENING FOR CONNECTIONS ON http://${HOST}:${PORT}`);
-  // schedule.start();
+    console.log("Scheduler initialized.");
+
+    app.listen(PORT, HOST, (error) => {
+      if (error) {
+        console.error("Unable to listen for HTTP connections", error);
+        console.error("This is usually due to permissions");
+        console.error("Run Powerbox as root or change port with PORT=8080 ./powerbox.js");
+        return void process.exit(102);
+      }
+
+      console.log(`Listening for connections on http://${HOST}:${PORT}`);
+    });
+  });
 });
